2fddf4c03f9c9a60cac66132e4c39694273d77b0
mikewyb <yw2303@nyu.edu>
2017-04-27 01:50:57 -0700

    inspect views

---
+++ frontend/background.js
@@ -0,0 +1,16 @@
+/*
+var queryInfo = {
+    active: true,
+    currentWindow: true
+};*/
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        // read `newIconPath` from request and read `tab.id` from sender
+        alert(request.newIconPath);
+        chrome.browserAction.setIcon({
+            path: request.newIconPath,
+            tabId: sender.tab.id
+        });
+    }
+);
