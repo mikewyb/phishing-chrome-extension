a4f0970e2468375fe4c7ab1812d31a177df6bc5b
mikewyb <yw2303@nyu.edu>
2017-04-27 01:52:53 -0700

    comment out alert

--- frontend/background.js
+++ frontend/background.js
@@ -7,7 +7,7 @@ var queryInfo = {
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         // read `newIconPath` from request and read `tab.id` from sender
-        alert(request.newIconPath);
+        //alert(request.newIconPath);
         chrome.browserAction.setIcon({
             path: request.newIconPath,
             tabId: sender.tab.id
